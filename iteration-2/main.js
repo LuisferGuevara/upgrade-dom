0aad7a9771a6c4f67e8918fa38e3a07c3a218a73
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-10-11 10:32:32 +0200

    finish exercise

--- iteration-2/main.js
+++ iteration-2/main.js
@@ -1,26 +1,77 @@
 // 2.1 Inserta dinamicamente en un html un div vacio con javascript.
-const div$$ = document.createElement('div');
+const div$$ = document.body.appendChild(document.createElement('div'));
+console.log(div$$);
 // 2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.
+const div1$$ = document.createElement('div');
 const p$$ = document.createElement('p');
-div$$.appendChild(p$$);
-document.body.appendChild(div$$)
-console.log(div$$)
+div1$$.appendChild(p$$);
+document.body.appendChild(div1$$);
 
 // document.body.appendChild(document.createElement("div")).appendChild(document.createElement("p"))
 
 
 // 2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
 
+const div3$$ = document.createElement('div');
+document.body.appendChild(div3$$)
+
+for (let i = 0; i < 6; i++) {
+    let sixPElements = document.createElement('p');   
+    div3$$.appendChild(sixPElements)
+}
+document.body.appendChild(div3$$)
+
 // 2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
+const p2$$ = document.createElement('p');
+p2$$.textContent = 'Soy dinámico';
+document.body.appendChild(p2$$)
 
 // 2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
+const h2$$ = document.querySelector('.fn-insert-here');
+
+h2$$.textContent = 'Wubba Lubba dub dub';
 
 // 2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
 // const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
 
-// 2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
+const socialList$$ = document.createElement('ul');
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
 
-// 2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
+for (let i = 0; i < apps.length; i++) {
+    console.log(apps[i])
+    let liElements = document.createElement('li');
+    liElements.textContent = apps[i];
+    socialList$$.appendChild(liElements)
+}
+
+document.body.appendChild(socialList$$);
+
+// 2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
+const allClassToRemove = document.querySelectorAll('.fn-remove-me')
+console.log(allClassToRemove);
+for (const element of allClassToRemove){
+    element.remove()
+}
+
+// 2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div.
 // 	Recuerda que no solo puedes insertar elementos con .appendChild.
 
+const p3$$ = document.createElement('p');
+const divs$$ = document.querySelector('div'); //Me duelve un array con dos posiciones
+p3$$.textContent = 'Voy en medio!';
+
+
+document.body.insertBefore(p3$$,divs$$[1]);// Con el insertBefore añado en la p antes que 
+                                          // la posicion 1 de mi array, es decir, al medio.
+
+ 
 // 2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here
+
+const findAllDivsWithClass$$ = document.querySelectorAll('.fn-insert-here');
+
+console.log(findAllDivsWithClass$$);
+for (let i = 1; i < findAllDivsWithClass$$.length; i++) {
+    let p4$$ = document.createElement('p');
+    p4$$.textContent = 'Voy dentro!';
+    findAllDivsWithClass$$[i].appendChild(p4$$)
+}
